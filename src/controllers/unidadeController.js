c468e8e2bb29712bb150b77181c3df86b236c0d7
M-Albertini <185203301+M-Albertini@users.noreply.github.com>
2026-06-07 15:36:12 -0300

    Add: Rotas para a tela de unidades
    Feitos a criação das rotas no MVC para a tela de unidades

--- src/controllers/unidadeController.js
+++ src/controllers/unidadeController.js
@@ -1,1 +1,41 @@
-// Unidade Controller
+exports.listarUnidades = (req, res) => {
+    const unidades = [];
+
+    res.render("unidades/index", {
+        unidades
+    });
+};
+
+exports.formNovaUnidade = (req, res) => {
+    res.render("unidades/nova");
+};
+
+exports.salvarUnidade = (req, res) => {
+    console.log(req.body);
+
+    res.redirect("/unidades");
+};
+
+exports.formEditarUnidade = (req, res) => {
+    const { id } = req.params;
+
+    res.render("unidades/editar", {
+        id
+    });
+};
+
+exports.atualizarUnidade = (req, res) => {
+    const { id } = req.params;
+
+    console.log("Atualizando:", id);
+
+    res.redirect("/unidades");
+};
+
+exports.excluirUnidade = (req, res) => {
+    const { id } = req.params;
+
+    console.log("Excluindo:", id);
+
+    res.redirect("/unidades");
+};
--- src/routes/unidadeRoutes.js
+++ src/routes/unidadeRoutes.js
@@ -1,1 +1,27 @@
-// Unidade Routes
+const express = require("express");
+const router = express.Router();
+
+const unidadeController = require("../controllers/unidadeController");
+
+console.log("CONTROLLER:");
+console.log(unidadeController);
+
+// Listar
+router.get("/", unidadeController.listarUnidades);
+
+// Form cadastro
+router.get("/nova", unidadeController.formNovaUnidade);
+
+// Salvar
+router.post("/salvar", unidadeController.salvarUnidade);
+
+// Form editar
+router.get("/editar/:id", unidadeController.formEditarUnidade);
+
+// Atualizar
+router.post("/editar/:id", unidadeController.atualizarUnidade);
+
+// Excluir
+router.post("/excluir/:id", unidadeController.excluirUnidade);
+
+module.exports = router;
